b7cc33579e3085d3ada062322e87561601189e14
Antonella <antonella.b95@hotmail.com>
2023-04-10 18:16:52 -0300

    ejercicios

---
+++ main.js
@@ -0,0 +1,61 @@
+// Ejercicio 1 
+
+function ejercicio1(num) {
+    
+    console.log(`El ${num} es impar`);  
+};
+
+ejercicio1("17")
+
+// Ejercicio 2 
+
+function ejercicio2(numero1, numero2) {
+
+if (numero1 > numero2) {
+    console.log(`El 2 es mayor que el 1`);
+}
+
+}
+
+ejercicio2 ("2", "1")
+
+// Ejercicio 3
+
+function ejercicio3 (numero3) {
+
+if (numero3 * 5 === 12) {
+    console.log(`Sí, el ${numero3} es múltiplo de 5`);
+}
+else {
+    console.log(`No, el ${numero3} no es múltiplo de 5`);
+}
+    
+}
+
+ejercicio3 ("12")
+
+// Ejercicio 4
+
+function ejercicio4 (numero4) {
+
+for (let i = 0; i < numero4; i++) {
+
+    if (i === 8) { continue;
+    }
+    
+console.log(`${i}`);    
+}
+
+} 
+
+ejercicio4 ("8")
+
+
+// Ejercicio 5 
+
+
+
+
+
+
+
